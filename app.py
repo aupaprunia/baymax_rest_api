27d0b290258cd8b25d30aa305b3a30002232479b
unknown <aupaprunia@gmail.com.com>
2020-03-29 17:21:21 +0530

    Heroku Files

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from security import authenticate,identity
 from resources.user import UserRegister
 from resources.patient import Patient,all_Patients
 from resources.hospital import Hospital, all_Hospitals
-from db import db
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
@@ -14,9 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 api=Api(app)
 app.secret_key="aditya"
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 jwt=JWT(app,authenticate,identity)
 
@@ -29,5 +26,6 @@ api.add_resource(all_Hospitals,'/allHospitals')
 api.add_resource(UserRegister,'/register')
 
 if __name__=='__main__':
+    from db import db
     db.init_app(app)
     app.run(debug=True,port=8000)
